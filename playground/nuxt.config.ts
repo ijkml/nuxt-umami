7a3ac1c1c43f116119f506bedfe002f63c61b07c
ml-laure <me.mlaure@gmail.com>
2022-09-16 14:24:54 +0100

    clean up comments

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,4 +1,3 @@
-import { defineNuxtConfig } from 'nuxt';
 import UmamiMetrics from '../src/module';
 
 export default defineNuxtConfig({
@@ -7,9 +6,6 @@ export default defineNuxtConfig({
   ],
   umami: {
     autoTrack: true,
-    // websiteId: 'ffcc51f0-9490-468a-bff9-2d0a0bb9ae60',
-    // scriptUrl: 'https://ijkml-umami.up.railway.app/umami.js',
-
     /* local */
     websiteId: '3c255b6d-678a-42dd-8074-272ee5b78484',
     scriptUrl: 'http://localhost:3000/umami.js',
