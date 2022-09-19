a6b2fb3271272c503b6e4094b56c219182d1f49e
ml-laure <me.mlaure@gmail.com>
2022-09-19 16:43:09 +0100

    add test examples of tracker

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
     autoTrack: true,
     /* local */
     websiteId: '3c255b6d-678a-42dd-8074-272ee5b78484',
-    scriptUrl: 'http://localhost:3000/umami.js',
+    scriptUrl: 'http://localhost:3001/umami.js',
     domains: '',
     cache: true,
   },
