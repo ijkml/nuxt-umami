df6e21188ac102ff62ead728dcc152ce0c8c6759
ML <me.mlaure@gmail.com>
2022-12-02 13:34:40 +0100

    playground: add every available option

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -5,11 +5,13 @@ export default defineNuxtConfig({
     UmamiMetrics,
   ],
   umami: {
-    autoTrack: true,
-    /* local */
-    websiteId: '3c255b6d-678a-42dd-8074-272ee5b78484',
-    scriptUrl: 'http://localhost:3001/umami.js',
-    domains: '',
-    cache: true,
+    websiteId: '3c255b6d-678a-42dd-8074-272ee5b78484', // string
+    scriptUrl: 'http://localhost:3001/umami.js', // string
+    // autoTrack: true, // bool
+    // domains: '', // string
+    // cache: true, // bool
+    // enable: false, // bool
+    // doNotTrack: false, // bool
+    // hostUrl: undefined, // string
   },
 });
