793206c8b72ac248c89720e709a84f9c17aca366
ML <me.mlaure@gmail.com>
2023-08-23 17:51:33 +0100

    update umami config

--- .playground/app.config.ts
+++ .playground/app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   umami: {
     host: 'https://ml-umami.netlify.app/',
-    id: '82d2c873-ad36-4ea3-9829-e283bc681a81',
+    id: '84cc2d28-8689-4df0-b575-2202e34a75aa',
     autoTrack: true,
     version: 2,
     useDirective: true,
