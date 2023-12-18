9729a7c0f89a453619996055fbb79933d4a4d0c6
ML <me.mlaure@gmail.com>
2023-12-18 14:40:39 +0100

    clean up

--- release.sh
+++ release.sh
@@ -9,5 +9,5 @@ npm publish --access public
 # Read the new version number from package.json
 version=$(node -p "require('./package.json').version")
 
-# Add a dist-tag to the new version
+# Add the `next` dist-tag to the new version
 npm dist-tag add nuxt-umami@$version next
